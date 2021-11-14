4a7c48b0145fc63394a2340a93c8a4c7a90f060e
kjameshuer <kjhuer@gmail.com>
2021-11-14 18:43:05 -0500

    future ideas

--- server/controllers/projects.js
+++ server/controllers/projects.js
@@ -3,9 +3,14 @@ const Project = require('../models/project');
 exports.newProject = function (req, res, next) {
     const project = new Project();
     const title = (req.body.title) ? req.body.title : 'My Project';
+    // can we get userId from server 
     const userId = (req.body.userId) ? req.body.userId : '2322332';
+    //
     project.userId = userId;
     project.title = title;
     project.save();
     return res.status(200).send(project);
 }
+
+//get project/s
+//save project
--- server/router.js
+++ server/router.js
@@ -10,6 +10,9 @@ module.exports = function (app) {
 
     app.post('/api/signup', Authentication.signup);
     app.post('/api/signin', requireSignin, Authentication.signin);
-    app.post('/api/project/new', requireSignin, Projects.newProject);
+    app.post('/api/project/new', requireAuth, Projects.newProject);
+    // app.get('/api/project/getProject', requireAuth, Projects.getProject)
+    // app.put('/api/project/saveProject', requireAuth, Projects.saveProject)
+
 
 }
