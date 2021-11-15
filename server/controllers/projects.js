d6fe90a3a7981fb02a6e04f70f23c013751ba286
kjameshuer <kjhuer@gmail.com>
2021-11-14 22:31:48 -0500

    added getProjects

--- server/controllers/projects.js
+++ server/controllers/projects.js
@@ -12,5 +12,10 @@ exports.newProject = function (req, res, next) {
     return res.status(200).send(project);
 }
 
+exports.getProjects = function (req, res, next) {
+    const data = [{ title: 'Project 1', id: 0 }, { title: 'Project 2', id: 1 }, { title: 'Project 3', id: 2 }]
+    return res.status(200).send(data)
+}
+
 //get project/s
 //save project
