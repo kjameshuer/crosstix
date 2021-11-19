e055d70a81a8f3bc1f94eba6c7ca912fb6a7efd5
kjameshuer <kjhuer@gmail.com>
2021-11-19 18:35:52 -0500

    initial style changes for grid

--- src/features/Builder/components/ColorSelector/ColorSelector.js
+++ src/features/Builder/components/ColorSelector/ColorSelector.js
@@ -3,28 +3,43 @@ import { HexColorPicker, HexColorInput } from "react-colorful";
 import { useDispatch, useSelector } from 'react-redux';
 import { updateRecentColors, setSelectedColor } from './colorSelectorSlice';
 import { addToProjectColors } from 'projectSlice'
+import './ColorSelector.scss';
 
 const ColorSelector = () => {
 
     const { selectedColor } = useSelector(state => state.colorInfo);
+    const [pickerVisible, setPickerVisible] = useState(true);
 
     const dispatch = useDispatch();
 
     const handleChange = col => {
         dispatch(setSelectedColor(col));
-        dispatch(updateRecentColors(col));
+        dispatch(updateRecentColors(col));        
     }
 
-    const handleClick = () => {
-        dispatch(addToProjectColors(selectedColor));
+    const handlePickerClick = () => {
+      //  if (pickerVisible) setPickerVisible(false)
+      //  else setPickerVisible(true);
     }
+    const displayPicker = () => {
+        return (
+            <div className="ColorSelector__picker">
+            <HexColorPicker color={selectedColor} onChange={handleChange} />
 
+            </div>
+        )
+    }
+    const buttonText = (pickerVisible) ? `Close` : `Select a colour`;
+    const colorStyle = {backgroundColor:selectedColor}
     return (
         <div className="ColorSelector">
-            <HexColorPicker color={selectedColor} onChange={handleChange} />
-            <HexColorInput color={selectedColor} onChange={handleChange} />
-            <br />
-            <button onClick={handleClick}>Add current color to Project Colors</button>
+            {displayPicker()}
+           {/* <button onClick={handlePickerClick}>{buttonText}</button> */}
+           <div className="ColorSelector__display">
+           <div onClick={handlePickerClick} style={colorStyle} className="ColorSelector__current-color"></div>
+           <HexColorInput color={selectedColor} onChange={handleChange} />
+           
+            </div>
         </div>
     )
 }
--- src/features/Builder/components/RecentColors/index.js
+++ src/features/Builder/components/RecentColors/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { useSelector, useDispatch } from 'react-redux';
-import { addToProjectColors } from 'features/Builder/components/ColorSelector/colorSelectorSlice';
+import { addToProjectColors } from 'projectSlice';
 import './RecentColors.scss';
 
 const RecentColors = () => {
